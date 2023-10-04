c44d42096e6df52a4f40ce63e4b37c04e43e3384
Tazzy2022 <micheletazbaz@gmail.com>
2023-10-04 16:01:56 -0700

    adding resto route

---
+++ server/api/restaurants.js
@@ -0,0 +1,8 @@
+//To authenticate API calls with the API Key, set the Authorization HTTP header value as Bearer API_KEY.
+const BASE_URL = "https://api.yelp.com/v3/businesses/"
+const router = require("express").Router();
+
+router.get()
+
+
+module.exports = router;
