b40b01d6f04fdf5d5fc68e297b2d87b09cb67ebc
Tazzy2022 <micheletazbaz@gmail.com>
2023-12-10 14:54:46 -0800

    updated packagejson

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,4 @@
 const db = require("./db");
-//const Follow = require("./models/Follow");
 const User = require("./models/User");
 const List = require("./models/List");
 const RestaurantNotes = require("./models/RestaurantNotes");
@@ -7,15 +6,11 @@ const Friend = require("./models/Friend");
 const Image = require("./models/Image")
 
 User.hasMany(List);
-// Follow.belongsTo(User);
-// User.hasMany(Follow, { foreignKey: "follower_id" });
 List.belongsTo(User);
 List.hasMany(RestaurantNotes);
 RestaurantNotes.belongsTo(List);
 Friend.belongsTo(User);
 User.hasMany(Friend, { foreignKey: "friendId" });
-// User.hasOne(Image)
-// Image.belongsTo(User)
 
 module.exports = {
   db,
@@ -23,5 +18,4 @@ module.exports = {
   RestaurantNotes,
   List,
   Friend,
- // Image
 };
