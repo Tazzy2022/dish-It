98e01de63acf6ba961c5e65ca89c8a8bc7fa983d
Tazzy2022 <micheletazbaz@gmail.com>
2023-12-17 15:32:47 -0800

    docker is working

--- server/auth/authRouter.js
+++ server/auth/authRouter.js
@@ -1,45 +1,39 @@
 const authRouter = require("express").Router();
 const User = require("../db/models/User");
-const Image = require("../db/models/Image");
 
 //POST '/auth/login'
-authRouter.post("/login", async (req, res, next) => {
+authRouter.post("/login", async (req, res) => {
   try {
-    const user = await User.authenticate(
-      req.body
-      //   , {
-      //   include: [{ model: Image }],
-      // }
-    );
-    res.send(user);
+    const user = await User.authenticate(req.body);
+    console.log("user in login", user)
+    return res.send(user);
   } catch (err) {
     res.status(500).json({
       message: "could not login user",
       err,
     });
-    next(err);
   }
 });
 
 //POST '/auth/signup'
-authRouter.post("/signup", async (req, res, next) => {
+authRouter.post("/signup", async (req, res) => {
   try {
     const checkUser = await User.findOne({ where: { email: req.body.email } });
-    if (checkUser) {
-      res.status(409).json({
+    if (!checkUser) {
+      const user = await User.encryptUser(req.body);
+      console.log("user in signup", user)
+      return res.send(user);
+    } else {
+      return res.status(409).json({
         message:
           "that email already exists, please login or use a different email",
       });
-    } else {
-      const user = await User.encryptUser(req.body);
-      res.send(user);
     }
   } catch (err) {
     res.status(500).json({
       message: "could not register user",
       err,
     });
-    next(err);
   }
 });
 
