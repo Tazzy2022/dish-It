c49969e8872f51d814f7b871142f78654053041c
Tazzy2022 <micheletazbaz@gmail.com>
2023-12-18 16:02:20 -0800

    removed jwt checks to debug

--- server/auth/authRouter.js
+++ server/auth/authRouter.js
@@ -16,22 +16,23 @@ authRouter.post("/login", async (req, res) => {
 
 //POST '/auth/signup'
 authRouter.post("/signup", async (req, res) => {
-  console.log("USER model", User);
   try {
     console.log("inside POST, req.body", req.body);
-    const checkUser = await User.findOne({ where: { email: req.body.email } });
-    if (!checkUser) {
-      console.log("checkUser in !checkUser", checkUser);
-      const user = await User.encryptUser(req.body);
-      console.log("user in signup", user);
-      return res.send(user);
-    } else {
-      console.log("checkUser in else", checkUser);
-      return res.status(409).json({
-        message:
-          "that email already exists, please login or use a different email",
-      });
-    }
+    const user = await User.create(req.body);
+    // const checkUser = await User.findOne({ where: { email: req.body.email } });
+    // if (!checkUser) {
+    //   console.log("checkUser in !checkUser", checkUser);
+    //   const user = await User.create(req.body)
+    // const user = await User.encryptUser(req.body);
+    // console.log("user in signup", user);
+    return res.send(user);
+    // } else {
+    //   console.log("checkUser in else", checkUser);
+    //   return res.status(409).json({
+    //     message:
+    //       "that email already exists, please login or use a different email",
+    //   });
+    // }
   } catch (err) {
     res.status(500).json({
       message: "could not register user",
@@ -41,3 +42,41 @@ authRouter.post("/signup", async (req, res) => {
 });
 
 module.exports = authRouter;
+
+//POST '/auth/login'
+// authRouter.post("/login", async (req, res) => {
+//   try {
+//     const user = await User.authenticate(req.body);
+//     return res.send(user);
+//   } catch (err) {
+//     res.status(500).json({
+//       message: "could not login user",
+//       err,
+//     });
+//   }
+// });
+
+//POST '/auth/signup'
+// authRouter.post("/signup", async (req, res) => {
+//   try {
+//     console.log("inside POST, req.body", req.body);
+//     const checkUser = await User.findOne({ where: { email: req.body.email } });
+//     if (!checkUser) {
+//       console.log("checkUser in !checkUser", checkUser);
+//       const user = await User.encryptUser(req.body);
+//       console.log("user in signup", user);
+//       return res.send(user);
+//     } else {
+//       console.log("checkUser in else", checkUser);
+//       return res.status(409).json({
+//         message:
+//           "that email already exists, please login or use a different email",
+//       });
+//     }
+//   } catch (err) {
+//     res.status(500).json({
+//       message: "could not register user",
+//       err,
+//     });
+//   }
+// });
