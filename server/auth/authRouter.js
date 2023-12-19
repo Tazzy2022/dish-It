385d400127b48627ee232a7b48bb128c4b899a9e
Tazzy2022 <micheletazbaz@gmail.com>
2023-12-19 14:25:01 -0800

    seeded render db

--- server/auth/authRouter.js
+++ server/auth/authRouter.js
@@ -20,20 +20,9 @@ authRouter.post("/signup", async (req, res) => {
     console.log("inside POST, req.body", req.body);
     //const user = await User.create(req.body);
     // console.log("user", user);
-    // const checkUser = await User.findOne({ where: { email: req.body.email } });
-    // if (!checkUser) {
-    //   console.log("checkUser in !checkUser", checkUser);
-    //   //const user = await User.create(req.body)
     const user = await User.encryptUser(req.body);
     //   console.log("user in signup", user);
     return res.send(user);
-    // } else {
-    //   console.log("checkUser in else", checkUser);
-    //   return res.status(409).json({
-    //     message:
-    //       "that email already exists, please login or use a different email",
-    //   });
-    // }
   } catch (err) {
     res.status(500).json({
       message: "could not register user",
